75f8ee4ae8e922836905856709ec28e7418b6dd6
Dylan <dkro8507@uni.sydney.edu.au>
2021-02-22 21:23:37 +0800

    portfolio

--- src/data/PortfolioData.js
+++ src/data/PortfolioData.js
@@ -7,12 +7,28 @@ export const PortfolioData = [
         github: "https://github.com/DylanKroft/myWebsite",
         demo: "https://dylank.netlify.app"
     },
+
     {
         title: "COVID-19 Vaccination Tracker", 
         date: "Jan 2021",
         pic: "ncovvaxtrack.png",
-        body: "A simple map visualizing the COVID-19 vaccination campaign worldwide.",
+        body: "An interactive map visualizing the COVID-19 vaccination campaign worldwide.",
         github: "https://github.com/DylanKroft/covid19VaccineTracker",
         demo: "https://ncovvaxtrack.com/"
     },
+
+    {
+        title: "Bin Chicken Run",
+        date: "Jul 2020",
+        pic: "binchicken.png",
+        body: "An endless-run style game I made as part of my learning the basics of Javascript.",
+        demo: "http://54.66.66.255/"
+    },
+
+    {
+        title: "NeoTimer",
+        date: "June 2020",
+        pic: "neotimer.png",
+        body: "A simple timer I made to try out neomorphism."
+    }
 ]
