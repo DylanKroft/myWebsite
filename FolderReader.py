ccd2fb80723449386a130fae8d289f5e68867966
Dylan <dkro8507@uni.sydney.edu.au>
2021-02-23 17:44:25 +0800

    file minimization

--- FolderReader.py
+++ FolderReader.py
@@ -5,7 +5,7 @@ tail = "];"
 
 for filename in os.listdir("static/photos"):
     if filename != ".DS_Store":
-        head += "\n{\nsrc: 'photos/%s',\nwidth: 1,\nheight: 1},\n" % filename
+        head += "\n{\nsrc: 'photos/%s',\nwidth: 3,\nheight: 4},\n" % filename
 
 head += tail
 
